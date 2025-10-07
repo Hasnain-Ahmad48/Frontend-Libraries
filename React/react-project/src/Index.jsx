e0d6412eede1ff5cc485399aefe1c37d3c4be629
Hasnain-Ahmad48 <hasnaink1480@gmail.com>
2025-10-08 00:00:09 +0500

    commit

--- React/react-project/src/Index.jsx
+++ React/react-project/src/Index.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // modern way (React 18+)
 
+// Component 1
 export default function Index1() {
   return (
     <div>
       <h1>Heading 1</h1>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, expedita!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, expedita!</p>
       <ul>
         <li>list items</li>
         <li>list items</li>
         <li>list items</li>
         <li>list items</li>
       </ul>
+      <div className="myDiv">
+        <h1>Add a class to this div</h1>
+      </div>
     </div>
-  )
+  );
 }
+
+// Component 2
+function MyComponent() {
+  return (
+    <div>
+      <h1>This is a simple component that returns an h1 tag</h1>
+    </div>
+  );
+}
+
+
+ReactDOM.render(<MyComponent />, document.getElementById("root"));
+
--- React/react-project/src/main.jsx
+++ React/react-project/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Index1 from './index.jsx'
+import Index1 from './Index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
